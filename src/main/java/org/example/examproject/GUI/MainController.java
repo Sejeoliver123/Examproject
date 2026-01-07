5370196f42fcde032646cc3ab1bcf526056d4e3e
CHRISSTOFFER\chris <chrisstofferb@gmail.com>
2026-01-07 11:05:23 +0100

    2nd window view added

--- src/main/java/org/example/examproject/GUI/MainController.java
+++ src/main/java/org/example/examproject/GUI/MainController.java
@@ -2,19 +2,26 @@ package org.example.examproject.GUI;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 public class MainController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
 
     @FXML
     private void onMovieAdd(ActionEvent actionEvent) {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/org/example/examproject/MovieCreation-view.fxml"));
+        try {
+            Scene scene = new Scene(fxmlLoader.load());
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+
+        }
+
     }
 
     @FXML
