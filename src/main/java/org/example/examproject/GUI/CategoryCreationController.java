918a00cfc43a216240e6f2ae546841b193dba12b
CHRISSTOFFER\chris <chrisstofferb@gmail.com>
2026-01-07 12:43:41 +0100

    addmovie window updated and addcategory window updated

--- src/main/java/org/example/examproject/GUI/CategoryCreationController.java
+++ src/main/java/org/example/examproject/GUI/CategoryCreationController.java
@@ -1,4 +1,12 @@
 package org.example.examproject.GUI;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class CategoryCreationController {
+    @FXML
+    private TextField txtCategoryTitle;
+    @FXML
+    private Button btnAddCategory;
 }
--- src/main/java/org/example/examproject/GUI/MovieCreationController.java
+++ src/main/java/org/example/examproject/GUI/MovieCreationController.java
@@ -1,4 +1,15 @@
 package org.example.examproject.GUI;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class MovieCreationController {
+    @FXML
+    private AnchorPane txtTitle;
+    @FXML
+    private TextField txtÅrstal;
+    @FXML
+    private Button btnAddMovie;
 }
