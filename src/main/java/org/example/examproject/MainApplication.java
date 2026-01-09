971e615af46ff702eaae6722b11e2fa7e2afe4d3
Kasper <kasand04@easv365.dk>
2026-01-09 11:58:07 +0100

    cross platform test

--- src/main/java/org/example/examproject/MainApplication.java
+++ src/main/java/org/example/examproject/MainApplication.java
@@ -12,7 +12,7 @@ public class MainApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("MoviePlayer!");
+        stage.setTitle("MoviePlayer! v2");
         stage.setScene(scene);
         stage.show();
     }
